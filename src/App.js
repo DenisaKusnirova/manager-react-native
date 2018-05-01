068e9b8aa3e4487ae1586091610a7c091358e319
Deniska Kusnirova <deniskakusnirova@gmail.com>
2018-05-01 12:14:09 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import firebase from 'firebase'
+import reducers from './reducers'
+
+class App extends Component {
+    componentWillMount() {
+        const config = {
+            apiKey: "AIzaSyCLyu6IdkDtZsBeADD2xCV_-SjkSG2Lzas",
+            authDomain: "manager-50627.firebaseapp.com",
+            databaseURL: "https://manager-50627.firebaseio.com",
+            projectId: "manager-50627",
+            storageBucket: "manager-50627.appspot.com",
+            messagingSenderId: "188094954957"
+          }
+          firebase.initializeApp(config)
+    }
+
+    render() {
+        return(
+            <Provider store={createStore(reducers)}>
+                <View>
+                    <Text>
+                        hello!
+                    </Text>
+                </View>
+            </Provider>
+        )
+    }
+}
+
+export default App
