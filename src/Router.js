2c8c5f7375e2a55016eb57c1092e083705aee822
Deniska Kusnirova <deniskakusnirova@gmail.com>
2018-05-02 16:30:43 +0200

    Set up a router component

---
+++ src/Router.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Scene, Router } from 'react-native-router-flux'
+import LoginForm from '../src/components/LoginForm'
+
+const RouterComponent = () => {
+    return (
+        <Router>
+            <Scene key="root">
+                <Scene key="login" component={LoginForm} title="Please login">
+                </Scene>
+            </Scene>
+        </Router>
+    )
+}
+
+export default RouterComponent
