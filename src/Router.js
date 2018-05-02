a05ce3ba69e39e1b73726774f8493c4fb0a2b9be
Deniska Kusnirova <deniskakusnirova@gmail.com>
2018-05-02 16:54:53 +0200

    Navigate between routes

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import { Scene, Router } from 'react-native-router-flux'
 import LoginForm from '../src/components/LoginForm'
+import EmployeeList from '../src/components/EmployeeList'
 
 const RouterComponent = () => {
     return (
         <Router>
-            <Scene key="root">
-                <Scene key="login" component={LoginForm} title="Please login">
+            <Scene key="root" hideNavBar>
+                <Scene key="auth">
+                    <Scene key="login" component={LoginForm} title="Please login" initial />
+                </Scene>
+                <Scene key="main" >
+                    <Scene key="employeeList" component={EmployeeList} title="Employees" />
                 </Scene>
             </Scene>
         </Router>
