b2cbaf127160523347ac736bf5c3a43cbda2179b
Deniska Kusnirova <deniskakusnirova@gmail.com>
2018-05-03 17:33:26 +0200

    Implement the picker

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Picker } from 'react-native'
 import { connect } from 'react-redux'
 import { employeeUpdate } from '../actions'
 import { Card, CardSection, Input, Button } from './common'
@@ -12,7 +13,7 @@ class EmployeeCreate extends Component {
                         label="Name"
                         placeholder="Jane"
                         value={this.props.name}
-                        onChangeText={value => this.props.employeeUpdate({ props: 'name', value })}
+                        onChangeText={value => this.props.employeeUpdate({ prop: 'name', value })}
                     />
                 </CardSection>
 
@@ -21,11 +22,24 @@ class EmployeeCreate extends Component {
                         label="Phone"
                         placeholder="555-555-555"
                         value={this.props.phone}
-                        onChangeText={value => this.props.employeeUpdate({ props: 'phone', value })}
+                        onChangeText={value => this.props.employeeUpdate({ prop: 'phone', value })}
                     />
                 </CardSection>
 
                 <CardSection>
+                    <Picker
+                        style={{ flex: 1 }}
+                        selectedValue={this.props.shift}
+                        onValueChange={value => this.props.employeeUpdate({ prop: 'shift', value })}
+                    >
+                        <Picker.Item label="Monday" value="Monday" />
+                        <Picker.Item label="Tuesday" value="Tuesday" />
+                        <Picker.Item label="Wednesday" value="Wednesday" />
+                        <Picker.Item label="Thursday" value="Thursday" />
+                        <Picker.Item label="Friday" value="Friday" />
+                        <Picker.Item label="Saturday" value="Saturday" />
+                        <Picker.Item label="Sunday" value="Sunday" />
+                    </Picker>
                 </CardSection>
 
                 <CardSection>
