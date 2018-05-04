ff04192a67e4461ca6daca47ffbe261c2cfd5d49
Deniska Kusnirova <deniskakusnirova@gmail.com>
2018-05-04 12:58:42 +0200

    Fetch data from firebase

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -2,9 +2,11 @@ import firebase from 'firebase'
 import { Actions } from 'react-native-router-flux'
 import { 
     EMPLOYEE_UPDATE,
-    EMPLOYEE_CREATE
+    EMPLOYEE_CREATE,
+    EMPLOYEES_FETCH_SUCCESS
  } from './types'
 
+ // Add a new employee:
 export const employeeUpdate = ({ prop, value }) => {
     return {
         type: EMPLOYEE_UPDATE,
@@ -12,6 +14,7 @@ export const employeeUpdate = ({ prop, value }) => {
     }
 }
 
+// Reset the from properties:
 export const employeeCreate = ({ name, phone, shift}) => {
     // currently logged in user:
     const { currentUser } = firebase.auth()
@@ -22,6 +25,18 @@ export const employeeCreate = ({ name, phone, shift}) => {
             .then(() => {
                 dispatch({ type: EMPLOYEE_CREATE })
                 Actions.pop({ type: 'reset' })
+            })
+    }
+}
+
+// Fetching the data:
+export const emmployeesFetch = () => {
+    const currentUser = firebase.auth()
+
+    return (dispatch) => {
+        firebase.database().ref(`/users/${currentUser.uid}/employees`)
+            .on('value', snapshot => {
+                dispatch({ type: EMPLOYEES_FETCH_SUCCESS, payload: snapshot.val()})
             })
     }
 }
